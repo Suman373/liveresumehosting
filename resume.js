d852b6761dd01da59ee60b09c0b3d1cf9eb0e815
Suman Roy <iamsuman898@gmail.com>
2025-08-29 14:12:38 +0530

    feat: resume updated

---
+++ resume.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+const simpleGit = require('simple-git');
+const { execSync } = require('child_process');
+
+const publicDirectory = path.join(__dirname, 'public');
+const htmlPath = path.join(__dirname, 'index.html');
+const scriptRegex = /<script>[\s\S]*?<\/script>/;
+const git = simpleGit();
+
+
+async function addChangesAndCommit() {
+  try {
+    await git.add('.');
+    await git.commit('feat: resume updated');
+    console.log('Changes added and committed.');
+  } catch (error) {
+    console.error(`Failed to add changes: ${error}`);
+  }
+}
+
+async function handleChangesAndVersionBump(releaseType) {
+  try {
+    console.log(`Bumping version with npm version ${releaseType}`);
+    execSync(`npm version ${releaseType}`, { stdio: 'inherit' });
+    console.log('Version successfully bumped.');
+  } catch (error) {
+    console.error(`Failed to bump version: ${error}`);
+    process.exit(1);
+  }
+}
+
+async function pushChangesToRemote() {
+  try {
+    console.log('Pushing commit to remote...');
+    await git.push('origin', 'main');
+    console.log('Successfully pushed changes to your repository.');
+  } catch (err) {
+    console.error(`An error occurred during git push: ${err}`);
+  }
+}
+
+async function parseResume() {
+  try {
+    console.log('Starting resume parsing...');
+    const files = fs.readdirSync(publicDirectory);
+    const file = files[0];
+    if (!file || file.length === 0) {
+      console.log("File not found.");
+      return;
+    }
+    console.log("Starting index.html parsing...");
+    const htmlData = fs.readFileSync(htmlPath, 'utf-8');
+    if (htmlData.match(scriptRegex)) {
+      const newScript = `<script>window.location.href='/public/${file}'</script>`;
+      const newContent = htmlData.replace(scriptRegex, newScript);
+      fs.writeFileSync(htmlPath, newContent);
+      console.log("HTML updated successfully");
+      addChangesAndCommit();
+      handleChangesAndVersionBump('patch'); // patch bumping for new resume files
+      pushChangesToRemote();
+    } else {
+      console.log("Could not parse HTML");
+      return;
+    }
+  } catch (error) {
+    console.log(`An error occured while parsing resume : ${error}`);
+  }
+}
+
+parseResume();
